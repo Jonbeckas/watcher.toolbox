8a6ad58e46c2d8ac2d914e4d9b0f14dd891cbd69
Jonbeckas <Jonasbeckerx3@outlook.de>
2019-11-20 17:09:03 +0100

    add getJarUrl()

--- src/main/java/net/tetraowl/watcher/toolbox/JavaTools.java
+++ src/main/java/net/tetraowl/watcher/toolbox/JavaTools.java
@@ -1,13 +1,29 @@
 package net.tetraowl.watcher.toolbox;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
+
 public class JavaTools {
     public static int javaMajorRelease() {
         String[] javaVersionElements = System.getProperty("java.runtime.version").split("\\.|_|-b");
         int major = Integer.parseInt(javaVersionElements[0]);
-        if (major>1) {
+        if (major > 1) {
             return major;
         } else {
-            return  Integer.parseInt(javaVersionElements[1]);
+            return Integer.parseInt(javaVersionElements[1]);
+        }
+    }
+
+    public static String getJarUrl(Class<?> context) {
+        URL url = context.getProtectionDomain().getCodeSource().getLocation();
+        try {
+            String string = URLDecoder.decode(url.getPath(), "UTF-8");
+            String tmp = string.replace("\\", "/");
+            int slash = string.lastIndexOf("/");
+            return string.substring(0, slash);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e.getMessage(), e);
         }
     }
 }
